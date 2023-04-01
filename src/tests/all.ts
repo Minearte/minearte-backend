73506ee19e4ef9249d1236560006dd3f32002cd7
Angelillo15 <angelrizosrubios@gmail.com>
2023-04-01 23:25:25 +0200

    Added tests

---
+++ src/tests/all.ts
@@ -0,0 +1,28 @@
+const fileNames: string[] = [];
+
+async function readDir(dir: string) {
+  for await (const dirEntry of Deno.readDir(dir)) {
+    if (
+      dirEntry.isFile &&
+      dirEntry.name.endsWith(".ts") &&
+      dirEntry.name !== "all.ts"
+    ) {
+      fileNames.push(dirEntry.name);
+    }
+
+    if(dirEntry.isDirectory){
+        readDir("src/tests/"+dirEntry.name)
+    }
+  }
+}
+
+await readDir("src/tests/")
+
+for (const fileName of fileNames) {
+  console.log(`Running ${fileName}...`);
+  const p = Deno.run({
+    cmd: ["deno", "test", "-A", `src/tests/${fileName}`],
+  });
+  p.status();
+}
+console.log(fileNames);
