fc34f860728efde0f33237f40c08b2cb92370e76
Angelillo15 <angelrizosrubios@gmail.com>
2023-04-01 22:53:18 +0200

    Remove unused import

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import server from "./server.ts";
 import logger from "./utils/logger.ts";
 import CacheManager from "./managers/CacheManager.ts";
-import axios from "./requests/axios.ts"
 
 logger.clear();
-
 await CacheManager.load();
 server.start();
