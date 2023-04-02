2b0f94a99778d6719d4079f0a728369c185a51d8
Angelillo15 <angelrizosrubios@gmail.com>
2023-04-02 14:31:08 +0200

    Added time to process the request

--- src/utils/routeLogger.ts
+++ src/utils/routeLogger.ts
@@ -4,9 +4,12 @@ import logger from './logger.ts';
 const router = Router();
 
 router.use(async function(req:Request, res:Response, next:NextFunction) {
+    const current = new Date();
     res.on('finish', await function() {
+        const finish = new Date();
+        const time = finish.getTime() - current.getTime();
         const status = res.statusCode ? res.statusCode : 200;
-        logger.info(`${logger.colors.blue} ${req.method} ${logger.colors.reset} ${req.path} ${logger.colors.yellow} ${status} ${logger.colors.reset}`);
+        logger.info(`${logger.colors.blue} ${req.method} ${logger.colors.reset} ${req.path} ${logger.colors.yellow} ${status} ${logger.colors.reset} ${time}ms`);
         next();
     });
     next();
