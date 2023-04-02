2e38d12fd959aaf186ee84b49d53558c248a0ad4
Angelillo15 <angelrizosrubios@gmail.com>
2023-04-02 14:29:42 +0200

    Added a schedule to refresh the cache

--- src/managers/CacheManager.ts
+++ src/managers/CacheManager.ts
@@ -4,6 +4,8 @@ import logger from "../utils/logger.ts";
 import AbstractCache from "../cache/AbstractCache.ts";
 import RedisCache from "../cache/RedisCache.ts";
 import InternalCache from "../cache/InternalCache.ts";
+import * as storeRequest from "../requests/store.ts";
+import "https://deno.land/x/dotenv@v3.2.2/load.ts";
 
 class loader {
     private static type:string;
@@ -15,11 +17,22 @@ class loader {
         if (this.type === "REDIS") {
             await RedisManager.connect();
             this.cacheClient = new RedisCache(RedisManager.getClient());
+            this.schelude();
             return
         }
 
         this.cacheClient = new InternalCache();
+        this.schelude();
         logger.info("Using memory cache (default).");
+
+    }
+
+    public static async schelude():Promise<void> {
+        await setInterval(async () => {
+            const categories = await storeRequest.getCategories();
+            await this.cacheClient.set("categories", JSON.stringify(categories));
+            logger.info("Cache refreshed!");
+        }, parseInt(Deno.env.get("CACHE_REFRESH") || "60000" ));
     }
 
     public static getCache():AbstractCache {
