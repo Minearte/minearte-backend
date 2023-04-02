9c4e04d6cf8956bbcf527c91d42c41a9799614d9
Angelillo15 <angelrizosrubios@gmail.com>
2023-04-02 15:18:43 +0200

    Fix import

--- src/cli/configure.ts
+++ src/cli/configure.ts
@@ -1,6 +1,6 @@
 import question from "https://raw.githubusercontent.com/ocpu/question-deno/master/mod.ts";
 import "https://deno.land/x/dotenv@v3.2.2/load.ts";
-import { load } from "https://deno.land/std/dotenv/mod.ts";
+import { load } from "https://deno.land/std@0.182.0/dotenv/mod.ts";
 import logger from "../utils/logger.ts";
 
 // Check if the .env file exists
@@ -10,7 +10,10 @@ const envExists = await Deno.stat(".env")
 
 if (envExists) {
   const result = await question("confirm", "The .env file already exists, you want to reconfigure?", false);
-  if (result === undefined) console.log("Prompt was canceled");
+  if (result === undefined) {
+    logger.info("Exiting...");
+    Deno.exit(0);
+  }
   else if (result) {
     Deno.remove(".env");
     logger.info("Removed .env file");
